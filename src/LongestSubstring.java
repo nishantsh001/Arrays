c74e250483d2ac6fc7befbc32e4ecf1e075f7594
nishantsh001 <nishant.sh1999@gmail.com>
2024-07-28 01:30:28 +0530

    new

--- src/LongestSubstring.java
+++ src/LongestSubstring.java
@@ -15,11 +15,13 @@ Input: s = "pwwkew"
 Output: 3
 Explanation: The answer is "wke", with the length of 3.
 Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.*/
+
 import java.util.HashMap;
 import java.util.Map;
 public class LongestSubstring {
 	public int lengthOfLongestSubstring(String s) {
 		int maxLength = 0;
+		
 		
 		//-->BruteForce O(n^2)
 		
@@ -30,29 +32,42 @@ public class LongestSubstring {
 //					break;
 //				}
 //				stringBuilder .append(s.charAt(j));
-////				if(stringBuilder.length()>maxLength) {
-////					maxLength=stringBuilder.length();
-////				}
+// //				if(stringBuilder.length()>maxLength) {
+// //					maxLength=stringBuilder.length();
+// //				}
 //				maxLength = Math.max(maxLength, stringBuilder.length());
 //				
 //				
 //			}
 //		}
 		
+		
+		
 		//-->Sliding window for O(n)
 		//Left only when we find repeating of substring 
 
-		Map<Character, Integer> visited = new HashMap<>();
+//		Map<Character, Integer> visited = new HashMap<>();
+//		for (int right = 0, left = 0; right < s.length(); right++) {
+//			
+//			if(visited.containsKey(s.charAt(right))&&visited.get(s.charAt(right))>=left){
+//				
+//				left = visited.get(s.charAt(right))+1;
+//			}
+//			
+//			maxLength = Math.max(maxLength, right-left+1 );
+//			visited.put(s.charAt(right), right);
+//		}
+		
+		
+		
 		for (int right = 0, left = 0; right < s.length(); right++) {
-			
-			if(visited.containsKey(s.charAt(right))&&visited.get(s.charAt(right))>=left){
-				
-				left = visited.get(s.charAt(right))+1;
+			int indexOfFirstAppearanceOnTheSubstring = s.indexOf(s.charAt(right), left);
+			if(indexOfFirstAppearanceOnTheSubstring != right) {
+				left = indexOfFirstAppearanceOnTheSubstring+1;
 			}
-			
-			maxLength = Math.max(maxLength, right-left+1 );
-			visited.put(s.charAt(right), right);
+			maxLength = Math.max(maxLength, right-left+1);
 		}
+			
 		return maxLength;
 	}
 	
